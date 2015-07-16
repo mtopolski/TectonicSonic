36f1f6a1ffc904f65641f575f5b85feb47ac6e76
Silly Knight <sillyknight1986@gmail.com>
2015-07-16 14:50:17 -0700

    Rename app module and delegate url routing to view module

--- client/app/app.js
+++ client/app/app.js
@@ -1,11 +1,9 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('app', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'app.view1'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
