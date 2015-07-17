49c54b2dec6d2dae985d312fc0d0ca3f058ac8e2
Silly Knight <sillyknight1986@gmail.com>
2015-07-16 20:05:04 -0700

    Temporary file upload

---
+++ server/config/middleware.js
@@ -0,0 +1,9 @@
+var morgan 	   = require('morgan'),
+	bodyParser = require('body-parser');
+
+module.exports = function(app, express) {
+	app.use(morgan('dev'));
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+	app.use(express.static(__dirname + '/../../app'));
+}
